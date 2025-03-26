a060b74bcbb0c257e777ac42726dcd87ed803e97
Akihito <antoinsta@gmail.com>
2025-03-26 16:59:17 +0100

    It's best if we tell the service worker how to work

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-        new workbox.strategies.CacheFirst()
+    ({ request }) => request.destination === 'image',
+    new workbox.strategies.CacheFirst()
 );
 
 const cacheName = "buldak-v1";
@@ -16,10 +16,26 @@ self.addEventListener("install", (e) => {
     console.log("[Service Worker] Install");
     e.waitUntil(
         (async () => {
-          const cache = await caches.open(cacheName);
-          console.log("[Service Worker] Caching all: app shell and content");
-          await cache.addAll(contentToCache);
+            const cache = await caches.open(cacheName);
+            console.log("[Service Worker] Caching all: app shell and content");
+            await cache.addAll(contentToCache);
         })(),
-      );
-  });
-  
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        (async () => {
+            const r = await caches.match(e.request);
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+            if (r) {
+                return r;
+            }
+            const response = await fetch(e.request);
+            const cache = await caches.open(cacheName);
+            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+            cache.put(e.request, response.clone());
+            return response;
+        })(),
+    );
+});
